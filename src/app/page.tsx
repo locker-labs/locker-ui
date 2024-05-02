77a32d3087ea955ba1e18eec699bfc542e3b76d6
0xernesto <78889960+0xernesto@users.noreply.github.com>
2024-05-02 04:47:11 -0500

    fix: lottie animation bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { useAuth } from "@clerk/nextjs";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 import AuthButton from "@/components/AuthButton";
-import LottieAnimation from "@/components/LottieAnimation";
 import { PATHS } from "@/data/paths";
+
+import animationLockerBlob from "../../public/assets/animationLockerBlob.json";
+
+const LottieAnimation = dynamic(() => import("@/components/LottieAnimation"), {
+	ssr: false,
+});
 
 export default function LandingPage() {
 	const router = useRouter();
@@ -20,32 +26,30 @@ export default function LandingPage() {
 
 	return (
 		<div className="flex w-full flex-1 flex-col items-center pt-16">
-			<div className="flex w-full max-w-2xl flex-col space-y-4">
+			<div className="flex w-full max-w-2xl flex-col space-y-10">
 				<h1 className="text-5xl">
 					<span className="bg-gradient-to-r from-secondary-200 to-primary-200 bg-clip-text text-transparent">
 						Automate
 					</span>{" "}
 					your crypto
 				</h1>
-				<span className="text-2xl text-dark-300 dark:text-light-300">
-					Set up your locker to automatically route funds to your
-					preferred channels every time you get paid on-chain.
+				<span className="text-2xl font-light text-dark-300 dark:text-light-500">
+					Save and invest every time you get paid on-chain. Set up
+					your locker today and watch your crypto go where you want
+					it.
 				</span>
-			</div>
-			<div className="mt-4 flex w-full max-w-2xl items-center text-xl">
-				<AuthButton
-					type="sign-up"
-					label="Sign up"
-					height="h-14"
-					width="w-40"
-				/>
+				<div className="flex w-full max-w-2xl items-center text-lg">
+					<AuthButton
+						type="sign-up"
+						label="Sign up"
+						height="h-12"
+						width="w-32"
+					/>
+				</div>
 			</div>
 			<div className="mt-14 flex w-full max-w-xl">
-				<LottieAnimation
-					animationUrl="/assets/animationLockerBlob.json"
-					loop
-					autoplay
-				/>
+				<LottieAnimation animationData={animationLockerBlob} />
+				{/* <LottieAnimation animationUrl="/assets/animationLockerBlob.json" /> */}
 			</div>
 		</div>
 	);
--- src/components/LottieAnimation.tsx
+++ src/components/LottieAnimation.tsx
@@ -1,42 +1,11 @@
-"use client";
-
 import Lottie from "lottie-react";
-import { useEffect, useState } from "react";
 
 export interface ILottieAnimation {
-	animationUrl: string;
-	loop: boolean;
-	autoplay: boolean;
+	animationData: Record<string, unknown>;
 }
 
-// TODO: Getting an "Internal Server Error" with code 500. Need to fix this.
-
-function LottieAnimation({
-	animationUrl,
-	loop = true,
-	autoplay = true,
-}: ILottieAnimation) {
-	const [animationData, setAnimationData] = useState<string>("");
-
-	useEffect(() => {
-		fetch(animationUrl)
-			.then((response) => response.json())
-			.then((data) => setAnimationData(data))
-			.catch((error) =>
-				// eslint-disable-next-line no-console
-				console.error("Error loading animation data:", error)
-			);
-	}, [animationUrl]);
-
-	return (
-		animationData && (
-			<Lottie
-				animationData={animationData}
-				loop={loop}
-				autoplay={autoplay}
-			/>
-		)
-	);
+function LottieAnimation({ animationData }: ILottieAnimation) {
+	return <Lottie animationData={animationData} />;
 }
 
 export default LottieAnimation;
