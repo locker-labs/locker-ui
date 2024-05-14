9b694e7ad25b5c6a71ea2804cd3cac681e1603a3
0xernesto <78889960+0xernesto@users.noreply.github.com>
2024-05-14 18:49:07 -0500

    feat: add testnet banner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,11 +11,12 @@ import ConnectButton from "@/components/ConnectButton";
 import HeaderMenu from "@/components/HeaderMenu";
 import ThemedImage from "@/components/ThemedImage";
 import { paths } from "@/data/constants/paths";
+import { supportedChainIds } from "@/data/constants/supportedChains";
 
 function Header() {
 	const pathname = usePathname();
 	const { isSignedIn } = useAuth();
-	const { isConnected } = useAccount();
+	const { isConnected, chainId } = useAccount();
 
 	const isAuthRoute =
 		pathname === paths.SIGN_IN || pathname === paths.SIGN_UP;
@@ -23,51 +24,66 @@ function Header() {
 	const showAuthButtons = !isSignedIn && !isAuthRoute;
 	const showConnectButton = isSignedIn && !isConnected;
 	const showMenu = isSignedIn && isConnected;
+	const showTestnetBanner =
+		isConnected &&
+		chainId &&
+		(chainId === supportedChainIds?.sepolia ||
+			chainId === supportedChainIds?.polygonMumbai ||
+			chainId === supportedChainIds?.avalancheFuji);
 
 	return (
-		<header className="top-0 z-10 flex h-20 w-full items-center justify-between">
-			<Link
-				className="relative mr-2 flex h-9 w-28 shrink-0 justify-center outline-none"
-				href={paths.LANDING}
-			>
-				<ThemedImage
-					darkImageSrc="/assets/logoLockerWhiteLetters.svg"
-					lightImageSrc="/assets/logoLockerDarkLetters.svg"
-					alt="Locker Logo"
-				/>
-			</Link>
-			{showMenu ? (
-				<div
-					className={`${pathname === paths.ACCOUNT && "hidden"} ml-2 flex items-center justify-center space-x-1`}
+		<header className="relative top-0 z-10 w-full">
+			{showTestnetBanner && (
+				<div className="w-full rounded-full bg-warning/20 p-2 text-center text-warning">
+					Testnet
+				</div>
+			)}
+			<div className="flex h-20 items-center justify-between">
+				<Link
+					className="relative mr-2 flex h-9 w-28 shrink-0 justify-center outline-none"
+					href={paths.LANDING}
 				>
-					<ChainDropdown />
-					<HeaderMenu />
-				</div>
-			) : showConnectButton ? (
-				<div className="ml-2 flex items-center justify-center xs1:space-x-2">
-					<div className="hidden xs1:flex">
-						<ConnectButton />
-					</div>
-					<HeaderMenu />
-				</div>
-			) : showAuthButtons ? (
-				<div className="ml-2 flex items-center justify-center sm:space-x-2">
-					<AuthButton
-						type="sign-in"
-						label="Sign in"
-						height="h-10"
-						width="w-24"
+					<ThemedImage
+						darkImageSrc="/assets/logoLockerWhiteLetters.svg"
+						lightImageSrc="/assets/logoLockerDarkLetters.svg"
+						alt="Locker Logo"
 					/>
-					<div className="hidden sm:flex">
+				</Link>
+				{showMenu ? (
+					<div
+						className={`${
+							pathname === paths.ACCOUNT && "hidden"
+						} ml-2 flex items-center justify-center space-x-1`}
+					>
+						<ChainDropdown />
+						<HeaderMenu />
+					</div>
+				) : showConnectButton ? (
+					<div className="ml-2 flex items-center justify-center xs1:space-x-2">
+						<div className="hidden xs1:flex">
+							<ConnectButton />
+						</div>
+						<HeaderMenu />
+					</div>
+				) : showAuthButtons ? (
+					<div className="ml-2 flex items-center justify-center sm:space-x-2">
 						<AuthButton
-							type="sign-up"
-							label="Sign up"
+							type="sign-in"
+							label="Sign in"
 							height="h-10"
 							width="w-24"
 						/>
+						<div className="hidden sm:flex">
+							<AuthButton
+								type="sign-up"
+								label="Sign up"
+								height="h-10"
+								width="w-24"
+							/>
+						</div>
 					</div>
-				</div>
-			) : null}
+				) : null}
+			</div>
 		</header>
 	);
 }
