5d674dd6a33c8f7caa06468d436ea12caa88404f
Marvin Arnold <marvin@geeky.rocks>
2024-09-02 20:44:36 -0500

    feat: enable phantom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { headers } from "next/headers";
 import { type ReactNode, Suspense } from "react";
-import { cookieToInitialState } from "wagmi";
 
 import Loading from "@/app/loading";
 import Footer from "@/components/Footer";
@@ -14,7 +13,6 @@ import { globalMetadata } from "@/data/seo/globalMetadata";
 import AuthProvider from "@/providers/AuthProvider";
 import EvmProvider from "@/providers/EvmProvider";
 import ThemeProvider from "@/providers/ThemeProvider";
-import { wagmiConfig } from "@/providers/wagmiConfig";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,18 +23,13 @@ export default function RootLayout({
 }: Readonly<{
 	children: ReactNode;
 }>) {
-	const initialState = cookieToInitialState(
-		wagmiConfig,
-		headers().get("cookie")
-	);
-
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body
 				className={`${inter.className} flex min-h-screen w-full flex-col items-center bg-light-100 text-dark-500 dark:bg-dark-500 dark:text-light-200`}
 			>
 				<AuthProvider>
-					<EvmProvider initialState={initialState}>
+					<EvmProvider cookie={headers().get("cookie") ?? ""}>
 						<ThemeProvider>
 							<main className="flex w-full min-w-[230px] max-w-5xl flex-1 flex-col items-center p-5">
 								<Header />
