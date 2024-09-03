df47cd822725029d0295c1f4b1976a52b47c4ffe
0xernesto <78889960+0xernesto@users.noreply.github.com>
2024-09-03 05:43:56 -0700

    fix: supabase client

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -3,37 +3,49 @@ import { createClient } from "@supabase/supabase-js";
 
 import { Database } from "./database.types";
 
-declare global {
-	interface Window {
-		Clerk: {
-			session?: {
-				getToken: (options: { template: string }) => Promise<string>;
-			};
-		};
-	}
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+	throw new Error("NEXT_PUBLIC_SUPABASE_URL is not defined");
+}
+if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+	throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY is not defined");
 }
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+// declare global {
+// 	interface Window {
+// 		Clerk: {
+// 			session?: {
+// 				getToken: (options: { template: string }) => Promise<string>;
+// 			};
+// 		};
+// 	}
+// }
+
+// function getSupabase() {
+// 	return createClient<Database>(supabaseUrl, supabaseAnonKey, {
+// 		global: {
+// 			fetch: async (url, options = {}) => {
+// 				const clerkToken = await window.Clerk.session?.getToken({
+// 					template: "supabase",
+// 				});
+
+// 				const headers = new Headers(options?.headers);
+// 				headers.set("Authorization", `Bearer ${clerkToken}`);
+
+// 				return fetch(url, {
+// 					...options,
+// 					headers,
+// 				});
+// 			},
+// 		},
+// 	});
+// }
+
+// NOTE: The above errors out. This, along with disabling RLS in Supabase, works.
 function getSupabase() {
-	return createClient<Database>(supabaseUrl, supabaseAnonKey, {
-		global: {
-			fetch: async (url, options = {}) => {
-				const clerkToken = await window.Clerk.session?.getToken({
-					template: "supabase",
-				});
-
-				const headers = new Headers(options?.headers);
-				headers.set("Authorization", `Bearer ${clerkToken}`);
-
-				return fetch(url, {
-					...options,
-					headers,
-				});
-			},
-		},
-	});
+	return createClient<Database>(supabaseUrl, supabaseAnonKey);
 }
 
 const supabaseClient = getSupabase();
