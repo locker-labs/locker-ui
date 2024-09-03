f56665b50fe3d2ce0a930ff5a0786c5fa3d5f13f
Marvin Arnold <marvin@geeky.rocks>
2024-09-02 20:44:36 -0500

    feat: enable phantom

--- src/providers/wagmiConfig.ts
+++ src/providers/wagmiConfig.ts
@@ -1,7 +1,40 @@
-import { cookieStorage, createConfig, createStorage } from "wagmi";
-import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
+"use client";
 
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import {
+	metaMaskWallet,
+	phantomWallet,
+	walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { cookieStorage, createConfig, createStorage } from "wagmi";
+
+// import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
 import { supportedChains, transports } from "@/data/constants/supportedChains";
+
+// const connectors = connectorsForWallets(
+// 	[
+// 		{
+// 			groupName: "Recommended",
+// 			wallets: [rainbowWallet, walletConnectWallet],
+// 		},
+// 	],
+// 	{
+// 		appName: "My RainbowKit App",
+// 		projectId: "YOUR_PROJECT_ID",
+// 	}
+// );
+
+const wallets = [
+	{
+		groupName: "Recommended",
+		wallets: [metaMaskWallet, walletConnectWallet, phantomWallet],
+	},
+];
+
+const connectors = connectorsForWallets(wallets, {
+	appName: "Locker",
+	projectId: process.env.WC_PROJECT_ID!,
+});
 
 export const wagmiConfig = createConfig({
 	ssr: true,
@@ -9,26 +42,6 @@ export const wagmiConfig = createConfig({
 		storage: cookieStorage,
 	}),
 	chains: supportedChains,
-	connectors: [
-		injected(),
-		walletConnect({
-			projectId: process.env.WC_PROJECT_ID!,
-			metadata: {
-				name: "Locker",
-				description: process.env.APP_DESCRIPTION!,
-				url: process.env.LANDING_PAGE_URL!,
-				icons: ["/assets/iconLocker.svg"],
-			},
-			qrModalOptions: {
-				themeMode: "light",
-			},
-		}),
-		coinbaseWallet({
-			appName: "Locker",
-			appLogoUrl: "/assets/iconLocker.svg",
-			darkMode: false,
-			preference: "eoaOnly",
-		}),
-	],
+	connectors,
 	transports,
 });
