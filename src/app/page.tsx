c38929f1fa51874c049424c4d4c2e7dbae59f00c
Marvin Arnold <marvin@geeky.rocks>
2024-09-04 22:41:13 -0500

    feat: updated designs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { SignUp, useAuth } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
+import AuthButton from "@/components/AuthButton";
 import FaqAccordion from "@/components/FaqAccordion";
+import {
+	IconArrowUpRight,
+	IconCustomize,
+	IconPaid,
+	IconPlus,
+} from "@/components/Icons";
 import StepInfo from "@/components/StepInfo";
 import { paths } from "@/data/constants/paths";
 import lockerPaths from "@/data/lottie/lockerPaths.json";
@@ -27,56 +34,69 @@ export default function LandingPage() {
 
 	return (
 		<div>
-			<div className="flex w-full flex-row gap-x-48">
+			<div className="flex w-full flex-row gap-x-36">
 				<div className="flex w-full max-w-2xl flex-col space-y-10">
 					<div>
-						<h1 className="mb-6 text-3xl dark:text-light-100">
+						<h1 className="mb-6 text-3xl">
 							<span className="bg-gradient-to-r from-secondary-200 to-primary-200 bg-clip-text text-transparent">
 								Save and invest
 							</span>{" "}
 							every time you get paid
 						</h1>
-						<span className="text-xl font-light text-dark-300 dark:text-light-200">
-							Automate your crypto. Save and invest every time you
-							get paid on-chain.
+						<span className="gray-600 text-xl">
+							Set up your locker and watch your crypto go where
+							you want it.
 						</span>
 					</div>
 
-					<div>
-						<p className="mb-6 font-bold">Three simple steps</p>
-						<div className="space-y-3">
-							<StepInfo text="1. Create a locker" icon="🔗" />
-							<StepInfo
-								text="2. Customize payment distributions"
-								icon="🔗"
-							/>
-							<StepInfo
-								text="3. Get paid at your locker"
-								icon="🔗"
-							/>
-						</div>
+					<div className="flex w-full max-w-2xl flex-row items-center space-x-4 text-lg">
+						<AuthButton
+							type="sign-up"
+							label="Sign up"
+							height="h-12"
+							width="w-full"
+						/>
+						<AuthButton
+							type="sign-in"
+							label="Sign in"
+							height="h-12"
+							width="w-full"
+						/>
+					</div>
+
+					<Link
+						href="https://docs.locker.money"
+						className="flex h-12 w-full items-center justify-center rounded-md text-center outline outline-gray-400"
+						target="_blank"
+					>
+						Documentation <IconArrowUpRight />
+					</Link>
+					<div className="flex flex-row space-x-3">
+						<StepInfo
+							text="Create a locker account"
+							icon={<IconPlus />}
+						/>
+						<StepInfo
+							text="Customize payment distributions"
+							icon={<IconCustomize />}
+						/>
+						<StepInfo
+							text="Get paid at your locker"
+							icon={<IconPaid />}
+						/>
 					</div>
 				</div>
-				<div className="flex w-full max-w-2xl flex-col items-center space-y-4">
-					<div className="flex w-2/3">
+				<div className="flex w-full max-w-2xl flex-col items-center space-y-8">
+					<div className="flex w-full">
 						<LottieAnimation animationData={lockerPaths} />
 					</div>
-					<SignUp />
+					<div className="flex w-full flex-col justify-center pb-10">
+						<p className="mb-5 text-xl dark:text-light-100">
+							Frequently asked questions
+						</p>
+						<FaqAccordion />
+					</div>
 				</div>
-			</div>
-			<Link
-				href="https://docs.locker.money"
-				className="h-12 w-full rounded-full text-center text-white"
-				target="_blank"
-			>
-				Documentation
-			</Link>
-
-			<div className="flex w-full flex-col justify-center pb-10">
-				<h1 className="mb-5 text-2xl dark:text-light-100">
-					Frequently asked questions
-				</h1>
-				<FaqAccordion />
 			</div>
 		</div>
 	);
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,52 +1,36 @@
-import Link from "next/link";
-import { FaTelegram, FaXTwitter } from "react-icons/fa6";
+import { FaGithub, FaTelegram, FaXTwitter } from "react-icons/fa6";
 
 import FarcasterIcon from "@/components/FarcasterIcon";
 import IconLink from "@/components/IconLink";
-import ThemeSwitcher from "@/components/ThemeSwitcher";
 
 function Footer() {
 	return (
 		<footer className="flex w-full flex-col items-center justify-center">
-			<div className="flex w-full flex-col items-center justify-center xs:flex-row xs:justify-between">
-				<div className="flex">
-					{/* <IconLink
+			<div className="flex">
+				<IconLink
 					Icon={FaGithub}
-					url={process.env.GITHUB_URL}
+					url="https://github.com/locker-labs"
 					label="Github Link"
 					size="20px"
-				/> */}
-					<IconLink
-						Icon={FarcasterIcon}
-						url={process.env.FARCASTER_URL}
-						label="Farcaster Link"
-						size="20px"
-					/>
-					<IconLink
-						Icon={FaXTwitter}
-						url={process.env.TWITTER_URL}
-						label="Twitter Link"
-						size="20px"
-					/>
-					<IconLink
-						Icon={FaTelegram}
-						url={process.env.TELEGRAM_URL}
-						label="Telegram Link"
-						size="20px"
-					/>
-				</div>
-				<div className="mt-4 flex xs:mt-0">
-					<ThemeSwitcher />
-				</div>
-			</div>
-			<div className="mt-6 flex text-center ">
-				<Link
-					href="https://docs.locker.money"
-					className="text-sm text-light-600"
-					target="_blank"
-				>
-					Docs
-				</Link>
+				/>
+				<IconLink
+					Icon={FarcasterIcon}
+					url={process.env.FARCASTER_URL}
+					label="Farcaster Link"
+					size="20px"
+				/>
+				<IconLink
+					Icon={FaXTwitter}
+					url={process.env.TWITTER_URL}
+					label="Twitter Link"
+					size="20px"
+				/>
+				<IconLink
+					Icon={FaTelegram}
+					url={process.env.TELEGRAM_URL}
+					label="Telegram Link"
+					size="20px"
+				/>
 			</div>
 			<div className="mt-6 flex text-center text-xs text-light-600">
 				<span className="text-sm">
