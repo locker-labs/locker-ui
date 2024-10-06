d0a1933cf977916730245971bfa43e39f11226a0
Marvin Arnold <marvin@geeky.rocks>
2024-10-05 22:47:20 -0500

    feat: restyle tx history

--- src/components/LockerPortfolioSavingsGoals.tsx
+++ src/components/LockerPortfolioSavingsGoals.tsx
@@ -3,13 +3,13 @@ import { PiggyBank } from "lucide-react";
 function LockerPortfolioSavingsGoals() {
 	return (
 		<div className="flex h-full w-full flex-col">
-			<p className="text-lg font-bold">Savings Goals</p>
+			<p className="font-bold">Savings Goals</p>
 			<div className="mt-3 flex h-full w-full flex-col items-center justify-center">
 				<div className="flex flex-col items-center justify-center space-y-2">
 					<p className="rounded-sm bg-gray-300 p-3 text-center text-white sm:p-5">
 						<PiggyBank size={28} />
 					</p>
-					<p>Coming soon!</p>
+					<p className="text-sm">Coming soon!</p>
 				</div>
 			</div>
 		</div>
